a8ac0fac10207a9e03234cf4ac22572cbde6d1f9
SUNGJUN WON <adrwss2@gmail.com>
2024-11-07 00:13:28 +0900

    Initial commit

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "apexcharts";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+  <>
+  </>
+  );
+
+  
+  export default Chart;
